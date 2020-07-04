dcfbe335223f75ed43b74aa6df2868bc928e86e5
HyunsuSo <hyunsuso@cclss.net>
2020-07-04 18:37:43 +0900

    di activity builder 생성

---
+++ app/src/main/java/com/sosu/gitrending/di/module/builder/ActivityBuilder.kt
@@ -0,0 +1,15 @@
+package com.sosu.gitrending.di.module.builder
+
+import com.sosu.gitrending.MainActivity
+import dagger.Module
+import dagger.android.ContributesAndroidInjector
+
+/**
+ * Created by hyunsuso on 2020/07/04.
+ */
+@Module
+abstract class ActivityBuilder {
+
+    @ContributesAndroidInjector
+    abstract fun contributeMainActivity(): MainActivity
+}
